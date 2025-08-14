dc323d8a9658973cbc078d673d826bb8ef4b8d8a
0CrazyLove <axelvasquez582@gmail.com>
2025-08-14 10:29:41 -0500

    ...

---
+++ Variables.py
@@ -0,0 +1,12 @@
+variable1 = "hola mundo"
+variable2 = 3
+variable3 = 3.3
+variable4 = True
+variable5 = [2, 4 , "hola", True, variable1]
+variable6 = None
+print(variable1)
+print(variable2)
+print(variable3)
+print(variable4)
+print(variable5[0])
+print(variable6)
