7021b647fd232b01df9a9cf2df91c156193a357a
0CrazyLove <axelvasquez582@gmail.com>
2025-08-14 14:04:19 -0500

    ...

---
+++ Validacion.py
@@ -0,0 +1,7 @@
+variable_one = 5
+if variable_one < 4:
+    print("es menor que 4")
+else:   
+        print("es mayor que 4") 
+
+print("pendejo")
---
+++ input.py
@@ -0,0 +1,3 @@
+numero_uno = int(input("introduce tu primer numero:"))
+numero_two = int(input("introduce tu primer numero:"))
+print(f"la suma de los numeros es:  {numero_uno + numero_two}")
